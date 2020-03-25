74fb56639284b55cc04983b133051f044c40203f
dannyt <cobreti_@hotmail.com>
2020-03-25 18:32:51 -0400

    updated scripts

--- jenkinsfile/authentication-init.groovy
+++ jenkinsfile/authentication-init.groovy
@@ -5,19 +5,38 @@ pipeline {
         }
     }
 
-    def image
-    
-    stage('checkout') {
-        git url: 'https://github.com/myportail/authentication'
-    }
-    
-    stage('build') {
-        image = docker.build("myportail/authentication-init:1.0.${env.BUILD_ID}", "-f ./Docker/authInit/Dockerfile .")
-    }
-    
-    stage('push') {
-        docker.withRegistry("", "dockerhub") {
-            image.push()
+    stages {
+        def image
+        
+        stage('checkout') {
+            agent any
+            steps {
+                    git url: 'https://github.com/myportail/authentication'
+            }
+        }
+        
+        stage('build') {
+            agent {
+                docker {
+                    reuseNode: true
+                }
+            }
+            steps {
+                image = docker.build("myportail/authentication-init:1.0.${env.BUILD_ID}", "-f ./Docker/authInit/Dockerfile .")
+            }
+        }
+        
+        stage('push') {
+            agent {
+                docker {
+                    reuseNode: true
+                }
+            }
+            steps: {
+                docker.withRegistry("", "dockerhub") {
+                    image.push()
+                }
+            }
         }
     }
 }
