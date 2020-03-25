c1f82169e7b4882ab6063a01323511ba0e4b8dcf
dannyt <cobreti_@hotmail.com>
2020-03-25 18:16:37 -0400

    added groovy scripts

---
+++ jenkinsfile/authentication-init.groovy
@@ -0,0 +1,19 @@
+pipeline {
+    node {   
+        def image
+        
+        stage('checkout') {
+            git url: 'https://github.com/myportail/authentication'
+        }
+        
+        stage('build') {
+            image = docker.build("myportail/authentication-init:1.0.${env.BUILD_ID}", "-f ./Docker/authInit/Dockerfile .")
+        }
+        
+        stage('push') {
+            docker.withRegistry("", "dockerhub") {
+                image.push()
+            }
+        }
+    }
+}
