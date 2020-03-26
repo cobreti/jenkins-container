fc93a235599339855f50e066b5b1091cac3fe684
dannyt <cobreti_@hotmail.com>
2020-03-26 09:58:52 -0400

    update

--- jenkinsfile/authentication-init.groovy
+++ jenkinsfile/authentication-init.groovy
@@ -1,25 +1,22 @@
 pipeline {
-    node any
-    
+    agent any
+    def image
+
     stages {
-        def image
 
         stage('checkout') {
-            agent any
             steps {
                     git url: 'https://github.com/myportail/authentication'
             }
         }
         
         stage('build') {
-            agent any
             steps {
                 docker.build("myportail/authentication-init:1.0.${env.BUILD_ID}", "-f ./Docker/authInit/Dockerfile .")
             }
         }
         
         stage('push') {
-            agent any
             steps: {
                 docker.withRegistry("", "dockerhub") {
                     image.push()
